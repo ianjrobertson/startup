bae106091c4abe4c52dcb956ccf6330092e0175e
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-10-29 22:32:50 -0600

    converted settings page

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 export function Settings() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Settings displayed here</div>
+    <main>
+      <h2>Settings</h2>
+        <form action="/change-username" method="POST">
+            <label for="username">Change Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter new username" required/>
+            <button type="submit">Update Username</button>
+          </form>
+        
+          <form action="/change-email" method="POST">
+            <label for="email">Change Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter new email" required/>
+            <button type="submit">Update Email</button>
+          </form>
+        
+          <form action="/change-password" method="POST">
+            <label for="password">Change Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter new password" required/>
+            <button type="submit">Update Password</button>
+          </form>
+        
+          <form action="/delete-account" method="POST">
+            <label for="delete-account">Delete Account</label>
+            <button type="submit">Delete My Account</button>
+          </form>
     </main>
   );
 }
