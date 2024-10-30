58e57cd5d828df601360d0ac13852dc6631f98c1
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-10-29 22:27:32 -0600

    createSpot page

--- src/createSpot/createSpot.jsx
+++ src/createSpot/createSpot.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export function CreateSpot() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>create spot displayed here</div>
+    <main>
+      <h2>Create Hammock Spot Post</h2>
+      <form action="/create-hammock-post" method="POST" enctype="multipart/form-data">
+      
+        <label for="name">Spot Name</label>
+        <input type="text" id="name" name="name" placeholder="Enter the spot name" required/>
+  
+      
+        <label for="location">Location</label>
+        <input type="text" id="location" name="location" placeholder="Enter the location or click on the map" required/>
+
+        <div>"Map API here"</div>
+  
+        <label for="image">Upload Image</label>
+        <input type="file" id="image" name="image" accept="image/*" required/>
+  
+        <label for="description">Description</label>
+        <textarea id="description" name="description" placeholder="Describe the hammock spot" required></textarea>
+  
+        <button type="submit">Post</button>
+      </form>
     </main>
   );
 }
