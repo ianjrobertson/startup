bf9bfec867e4ab35dfc97075ba4aa05460686aad
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-10-29 22:31:06 -0600

    Converted saved page

--- src/saved/saved.jsx
+++ src/saved/saved.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React from "react";
 
 export function Saved() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Saved displayed here</div>
+    <main>
+      <h2>From Database: Saved Spots</h2>
+      <div className="saved-container">
+        <table className="saved-table">
+          <thead>
+            <tr>
+              <th>Hammock Spot</th>
+              <th>Username</th>
+              <th>Location</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  src="https://thehammockexpert.com/wp-content/uploads/2018/01/how-to-hang-your-hammock-the-hammock-expert-3-1-768x512.jpg"
+                  alt="Hammock Spot"
+                  width="100"
+                />
+              </td>
+              <td>"Username here"</td>
+              <td>
+                <a href="https://maps.google.com" target="_blank">
+                  Use Map API: View Location
+                </a>
+              </td>
+              <td class="delete-action">X</td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src="https://thehammockexpert.com/wp-content/uploads/2018/01/how-to-hang-your-hammock-the-hammock-expert-3-1-768x512.jpg"
+                  alt="Hammock Spot"
+                  width="100"
+                />
+              </td>
+              <td>"Username here"</td>
+              <td>
+                <a href="https://maps.google.com" target="_blank">
+                  Use Map API: View Location
+                </a>
+              </td>
+              <td className="delete-action">X</td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src="https://thehammockexpert.com/wp-content/uploads/2018/01/how-to-hang-your-hammock-the-hammock-expert-3-1-768x512.jpg"
+                  alt="Hammock Spot"
+                  width="100"
+                />
+              </td>
+              <td>"Username name"</td>
+              <td>
+                <a href="https://maps.google.com" target="_blank">
+                  Use Map API: View Location
+                </a>
+              </td>
+              <td className="delete-action">X</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
