c3c22f441d7af870257157dd703dd4e517411484
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-11-16 22:06:11 -0700

    created getSaved

--- service/index.js
+++ service/index.js
@@ -48,7 +48,7 @@ apiRouter.post('/auth/login', async (req, res) => {
   });
 
   //createPost
-  apiRouter.post('/auth/createPost', async (req, res) => {
+  apiRouter.post('/create', async (req, res) => {
     const user = Object.values(users).find((u) => u.token === req.body.token);
     if (user) {
       let post = {user: user.email, postID: req.body.postID, name: req.body.name, location: req.body.location, image: req.body.image, description: req.body.description}
@@ -66,7 +66,7 @@ apiRouter.post('/auth/login', async (req, res) => {
   })
 
   //likePost
-  apiRouter.post('/auth/likePost', async (req, res) => {
+  apiRouter.post('/like', async (req, res) => {
     const user = Object.values(users).find((u) => u.token === req.body.token);
     if (user) {
       if (!likedPosts[user.email]) {
@@ -80,7 +80,7 @@ apiRouter.post('/auth/login', async (req, res) => {
   })
 
   //savePost
-  apiRouter.post('/auth/savePost', async (req, res) => {
+  apiRouter.post('/save', async (req, res) => {
     const user = Object.values(users).find((u) => u.token === req.body.token);
     if (user) {
       if (!savedPosts[user.email]) {
@@ -92,6 +92,18 @@ apiRouter.post('/auth/login', async (req, res) => {
     }
     res.status(200).send({ message: "Post Liked successfully" });
   })
+
+  //getSaved
+  apiRouter.get('/saved', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      res.send(savedPosts[user.email])
+    } else {
+      return res.status(401).send({ error: "Invalid token or user not found" });
+    }
+  })
+  
+  
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
