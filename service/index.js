09fb6c8c8d069046c12e0cbe88065bd695552047
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-11-16 21:52:05 -0700

    created savePost service

--- service/index.js
+++ service/index.js
@@ -6,8 +6,10 @@ app.use(express.json());
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 let users = {}; // We will replace this cached value with db in the next phase. 
-let scores = [];
-let posts = {}
+let posts = {};
+let likedPosts = {};
+let savedPosts = {};
+
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
@@ -49,7 +51,7 @@ apiRouter.post('/auth/login', async (req, res) => {
   apiRouter.post('/auth/createPost', async (req, res) => {
     const user = Object.values(users).find((u) => u.token === req.body.token);
     if (user) {
-      let post = {user: user.email, name: req.body.name, location: req.body.location, image: req.body.image, description: req.body.description}
+      let post = {user: user.email, postID: req.body.postID, name: req.body.name, location: req.body.location, image: req.body.image, description: req.body.description}
       if (posts[user.email]) {
         posts[user.email].push(post)
       }
@@ -62,7 +64,21 @@ apiRouter.post('/auth/login', async (req, res) => {
     }
     res.status(200).send({ message: "Post created successfully" });
   })
+
   //likePost
+  apiRouter.post('/auth/likePost', async (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      if (!likes[user.email]) {
+        likes[email] = new Set();
+      }
+      likes[email].add(postID);
+    } else {
+      return res.status(401).send({ error: "Invalid token or user not found" });
+    }
+    res.status(200).send({ message: "Post Liked successfully" });
+  })
+
   //savePost
 
 app.listen(port, () => {
