f7c7bb91bafaa1fcf3b7f7d7b666ef7022b328c7
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-11-21 22:07:20 -0700

    Refactoring Login

--- src/main/login/login.jsx
+++ src/main/login/login.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <div className="accordion" id="loginAccordion">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingLogin">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLogin" aria-expanded="true" aria-controls="collapseLogin">
-              Login
-            </button>
-          </h2>
-          <div id="collapseLogin" className="accordion-collapse" aria-labelledby="headingLogin" data-bs-parent="#loginAccordion">
-            <div className="accordion-body">
-              <form action="/login" method="POST">
-                <div className="mb-3">
-                  <label htmlFor="username" className="form-label">Username</label>
-                  <input type="text" className="form-control" id="username" name="username" placeholder="Enter username" required/>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">Password</label>
-                  <input type="password" className="form-control" id="password" name="password" placeholder="Enter password" required/>
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-              </form>
-            </div>
-          </div>
-        </div>
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Hangspot</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
       </div>
+    </main>
   );
 }
--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Login } from './login/login';
 import { Spot } from "./spot/spot"
+import { AuthState } from './login/authState';
 
 export function Main() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
   return (
     <main>
-      <Login></Login>
-      <Spot></Spot>
-      <Spot></Spot>
-      <Spot></Spot>
-      <Spot></Spot>
+      <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
     </main>
   );
 }
