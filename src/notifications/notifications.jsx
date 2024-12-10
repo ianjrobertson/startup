0f7c0b2cc1d87a50a5b727390141a37a99e27ac3
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-12-09 21:48:29 -0700

    Edited notifications file

--- src/notifications/notifications.jsx
+++ src/notifications/notifications.jsx
@@ -1,129 +1,49 @@
 import React from 'react';
 
+import { HangspotEvent, notifier } from './notifier';
+
+
 export function Notifications() {
+  const userName = localStorage.getItem('userName');
+  const [events, setEvent] = React.useState([]);
+  React.useEffect(() => {
+    notifier.addHandler(handleHangspotEvent);
+
+    return () => {
+      notifier.removeHandler(handleHangspotEvent)
+    }
+  })
+
+  function handleHangspotEvent(event) {
+    setEvent([...events, event])
+  }
+
+  function createMessageArray() {
+    const messageArray = [];
+    for (const [i, event] of events.entries()) {
+      let message = 'unknown';
+      if (event.type === HangspotEvent.Like) {
+        message = `liked ${event.value.user}`;
+      } else if (event.type === HangspotEvent.Save) {
+        message = `saved ${event.value.title}`;
+      } else if (event.type === HangspotEvent.Spot) {
+        message = `made new spot`;
+      } else if (event.type === HangspotEvent.System) {
+        message = event.value.msg;
+      }
+      messageArray.push(
+        <div key={i}>
+          <span>{event.from.split('@')[0]}</span>
+          {message}
+        </div>
+      )
+    }
+    return messageArray;
+  }
   return (
-    <main>
-      <h2>Notifications</h2>
-    <div className="notification-table">
-      <table>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="profile"
-                src="IMG_1840.jpg"
-                height="30px"
-                width="35px"
-              />
-              "Username"
-            </td>
-            <td>"Action"</td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <span>{userName}</span> 
+      <div>{createMessageArray}</div>
     </div>
-    </main>
   );
 }
